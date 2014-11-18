f54830da46fb889e9a8343179867954c3f43777e
hame22 <alex.hamer@gmail.com>
2014-11-18 19:55:02 +0000

    Test component

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ import * as fs from "fs";
 import * as jade from "jade";
 import * as path from "path";
 import * as React from "react";
+import {default as list} from "./components/list.jsx"
 
 export let app = new Express();
 
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-//app.use(Express.static(path.join(__dirname, 'public')));
+app.use(Express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res, next) => {
-	res.send('hello world')
+let locals = {
+    props: {
+      state: {text: "hello world", items: ['item1', 'item2']}
+    }
+  };
+  locals.content = React.renderToString(React.createFactory(list)(locals.props));
+  res.render('layout', locals);
 });
 
 app.listen(1919);
---
+++ components/list.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default React.createClass({
+	displayName: 'list',
+
+	render: function () {
+		return (
+			<h2>My component</h2>
+			)
+	}
+});
