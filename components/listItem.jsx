e4df1d0bae19c404ae980141f5ff44f8a39424ef
hame22 <alex.hamer@gmail.com>
2014-11-20 22:32:37 +0000

    additional restaurant attributes

--- components/listItem.jsx
+++ components/listItem.jsx
@@ -4,8 +4,13 @@ export default React.createClass({
 	displayName: 'listItem',
 
 	render: function () {
+		let item = this.props.restaurant;
 		return (
-			<li>{this.props.restaurant.Name}</li>
+			<li>
+				<img src={item.LogoUrl} />
+				<h2>{item.Name}</h2>
+				<p>{item.Address.FirstLine}, {item.Address.City}, {item.Address.Postcode}</p>
+			</li>
 			)
 	}
 });
